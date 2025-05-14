6f79e4bf36214b8293c1e056c2f4edd7a991cd6e
Tejas Tidake <tejas.tidake@coditas.com>
2025-05-14 23:55:46 +0530

    Listing Datasource

---
+++ lib/features/product_listing/data/datasource/listing_datasource.dart
@@ -0,0 +1,40 @@
+import 'package:api_clean_architecture/core/networking/failure.dart';
+import 'package:api_clean_architecture/core/networking/network_constants.dart';
+import 'package:api_clean_architecture/core/networking/network_service.dart';
+import 'package:api_clean_architecture/core/utils/enums/method_enum.dart';
+import 'package:api_clean_architecture/features/product_listing/data/model/listing_model.dart';
+import 'package:api_clean_architecture/features/product_listing/domain/entity/listing_entity.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+abstract interface class ListingDatasource {
+  Future<Either<Failure, List<ListingEntity>>> getAllProducts();
+}
+
+class ListDataSourceImpl implements ListingDatasource {
+  final NetworkService networkService;
+  ListDataSourceImpl(this.networkService);
+
+  @override
+  Future<Either<Failure, List<ListingEntity>>> getAllProducts() async {
+    try {
+      final result = await networkService.request(
+        NetworkConstants.productEndpoint,
+        MethodEnum.get,
+      );
+
+      return result.fold((failure) => Left(failure), (response) {
+        if (response.data is Map<String, dynamic>) {
+          final List data = response.data['products'] ?? [];
+          final products =
+              data.map((product) => ListingModel.fromJson(product)).toList();
+          return Right(products);
+        } else {
+          return Left(CustomError('Unsupported Format'));
+        }
+      });
+    } on DioException catch (e) {
+      return Left(CustomError(e.toString()));
+    }
+  }
+}
